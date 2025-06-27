02e11dc382e49972029710d1896ad1d0d8b3036e
utubo <utbutb@gmail.com>
2025-06-28 02:26:33 +0900

    🎉release:

---
+++ src/background.js
@@ -0,0 +1,85 @@
+"use strict";
+browser.menus.create({
+	id: 'bookmark_query_add',
+	contexts: ['bookmark'],
+	title: browser.i18n.getMessage('Add query'),
+});
+browser.menus.create({
+	id: 'bookmark_query_edit',
+	contexts: ['bookmark'],
+	title: browser.i18n.getMessage('Edit'),
+	visible: false,
+});
+
+let bookmarkId;
+let menuItemId;
+
+browser.menus.onShown.addListener(async info => {
+	if (info.contexts[0] !== 'bookmark') return;
+	const [bookmark] = await browser.bookmarks.get(info.bookmarkId);
+	browser.menus.update('bookmark_query_edit', {
+		visible: bookmark.type === 'bookmark' &&  bookmark.url.startsWith('place:')
+	});
+	browser.menus.refresh();
+});
+
+browser.menus.onClicked.addListener(info => {
+	bookmarkId = info.bookmarkId;
+	menuItemId = info.menuItemId;
+	browser.action.openPopup();
+});
+
+const createTree = nodes => {
+	const tree = [];
+	for (const node of nodes ?? []) {
+		if (node.type !== 'folder') continue;
+		tree.push({
+			id: node.id,
+			title: node.title,
+			children: createTree(node.children),
+		});
+	}
+	return tree;
+}
+
+const createParams = async () => {
+	const [bookmark] = await browser.bookmarks.get(bookmarkId);
+	const tree = createTree(await browser.bookmarks.getTree());
+	tree[0].title = browser.i18n.getMessage('all_bookmarks');
+	const parentId = bookmark.type === 'folder'
+		? bookmark.id
+		: bookmark.parentId ?? tree[0].id;
+	return {
+		bookmark: {
+			parentId: parentId,
+			id: menuItemId === 'bookmark_query_edit' ? bookmark.id : '',
+			title: bookmark.title,
+			url: bookmark.url,
+		},
+		tree: tree,
+	};
+}
+
+const msgHandler = async (msg, _, sendResponse) => {
+	switch (msg.method) {
+		case 'get':
+			sendResponse(createParams());
+			break;
+		case 'put':
+			if (msg.bookmark.id) {
+				browser.bookmarks.update(msg.bookmark.id, {
+					title: msg.bookmark.title,
+					url: msg.bookmark.url,
+				});
+			} else {
+				browser.bookmarks.create(msg.bookmark);
+			}
+			sendResponse(null);
+			break;
+	}
+};
+
+browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+	msgHandler(msg, sender, sendResponse);
+	return true;
+});
---
+++ src/popup.js
@@ -0,0 +1,112 @@
+document.documentElement.lang = navigator.language;
+for (const e of document.getElementsByClassName('i18n')) {
+  e.textContent = browser.i18n.getMessage(e.textContent) ?? e.textContent;
+}
+
+let params;
+const $title = document.getElementById('title');
+const $parent = document.getElementById('parent');
+const $sort = document.getElementById('sort');
+const $query = document.getElementById('query');
+const $tag = document.getElementById('tag');
+
+const addTree = (tree, f, indent) => {
+  if (!tree) return;
+  for (const t of tree) {
+    const o = document.createElement('OPTION');
+    o.value = t.id;
+    o.textContent = `${indent}📂${t.title}`;
+    f.appendChild(o);
+    addTree(t.children, f, indent + '\u2003');
+  }
+}
+
+let isAutoTitle = false;
+
+const createTitle = () => {
+  const items = [];
+  for (const id of ['sort', 'query', 'tag']) {
+    const f = document.getElementById(id);
+    if (!f?.value) continue;
+    if (id === 'sort') {
+      const fmt = browser.i18n.getMessage('sort by');
+      const value = f.options[f.selectedIndex].text;
+      items.push(fmt.replace(/value/, value));
+    } else {
+      const name = browser.i18n.getMessage(id);
+      items.push(`${name}:${f.value}`);
+    }
+  }
+  return items.join(', ');
+}
+
+const autoTitle = () => {
+  if (!isAutoTitle) return;
+  $title.value = createTitle();
+}
+
+const checkTitle = () => {
+  isAutoTitle = !$title.value || $title.value === createTitle();
+}
+
+// Main
+
+const init = async () => {
+  params = await browser.runtime.sendMessage({ method: 'get' });
+
+  // Setup parent tree
+  const f = document.createDocumentFragment();
+  addTree(params.tree, f, '');
+  $parent.appendChild(f);
+
+  // Setup default parameters
+  $parent.value = params.bookmark.parentId;
+  if (params.bookmark.id) {
+    $title.value = params.bookmark.title;
+    const q = new URLSearchParams(params.bookmark.url.replace('place:', ''));
+    const p = {};
+    for (const [k, v] of q.entries()) {
+      p[k] = v;
+      const f = document.getElementById(k);
+      if (f) {
+        f.value = v;
+      }
+    }
+    if (p.type === '2') {
+      $parent.value = 'history';
+    }
+  }
+  checkTitle();
+
+  // Event handler
+  document.getElementById('submit').addEventListener('click', async () => {
+    const q = {};
+    for (const i of document.getElementsByClassName('q')) {
+      if (i.value) q[i.id] = i.value;
+    }
+    if (q.parent === 'history') {
+      q.type = '2';
+      delete q.parent;
+    }
+    const bookmark = {
+      parentId: params.bookmark.parentId,
+      title: $title.value,
+      url: 'place:' + (new URLSearchParams(q)).toString(),
+    };
+    if (params.bookmark?.id) {
+      bookmark.id = params.bookmark.id;
+    }
+    await browser.runtime.sendMessage({
+      method: 'put',
+      bookmark: bookmark,
+    });
+    close();
+  });
+
+  $title.addEventListener('input', checkTitle);
+  $sort.addEventListener('change', autoTitle);
+  $query.addEventListener('input', autoTitle);
+  $tag.addEventListener('input', autoTitle);
+};
+
+init();
