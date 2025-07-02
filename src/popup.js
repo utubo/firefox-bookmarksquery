c736260c1c76077e67b7a05ab8373c3ed91cf06b
utubo <utbutb@gmail.com>
2025-07-02 11:58:52 +0900

    ✨feat: Support multiple search targets, close #2

--- src/popup.js
+++ src/popup.js
@@ -5,8 +5,9 @@ for (const e of document.getElementsByClassName('i18n')) {
 
 const byId = id => document.getElementById(id);
 
-let params;
+let popupArgs;
 let isAutoTitle = false;
+let selectedParents = [];
 const $title = byId('title');
 const $parent = byId('parent');
 const $sort = byId('sort');
@@ -14,39 +15,55 @@ const $query = byId('query');
 const $tag = byId('tag');
 const $bookmarkURL = byId('bookmarkURL');
 
-const addTree = (tree, f, parent = null, indent = '') => {
+// Folder tree
+const addTree = (tree, f, parentNode = null, indent = '') => {
   if (!tree) return;
   for (const t of tree) {
     const o = document.createElement('OPTION');
     o.value = t.id;
+    o.id = `p-${t.id}`;
     o.textContent = `${indent}📂${t.title}`;
-    o.setAttribute('data-parent', parent?.id);
-    o.style.display = (!parent || parent.opened) ? 'block' : 'none';
+    o.setAttribute('data-parent', parentNode?.id);
+    o.style.display = !parentNode ? 'block' : 'none';
     f.appendChild(o);
     addTree(t.children, f, t, indent + '\u2003');
   }
 };
 
-const openTree = () => {
-  let i = $parent.selectedIndex;
-  while (true) {
-    i ++;
-    const o = $parent.options[i];
-    if (o?.getAttribute('data-parent') !== $parent.value) return;
+const openTree = id => {
+  const t = byId(`p-${id}`);
+  if (!t) return {};
+  if (t.getAttribute('data-open')) return t;
+  t.setAttribute('data-open', true);
+  const p = t.getAttribute('data-parent');
+  const children = Array.from($parent.options)
+    .filter(option => option.getAttribute('data-parent') === id);
+  for (const o of children) {
     o.style.display = 'block';
   }
-}
+  openTree(p);
+  return t;
+};
 
-const findParent = tree => {
-  if (!tree) return;
-  for (const t of tree) {
-    if (t.id === params.bookmark.parentId || findParent(t.children)) {
-      t.opened = true;
-      return true;
-    }
+const setParentMultiple = b => {
+  if (b) {
+    $parent.setAttribute('multiple', 'multiple');
+  } else {
+    $parent.removeAttribute('multiple');
   }
-}
+};
 
+const onSelectParent = () => {
+  let i = $parent.selectedIndex;
+  setParentMultiple(2 < i);
+  selectedParents = Array.from($parent.options)
+    .filter(option => option.selected)
+    .map(option => option.value);
+  setBookmarkURL();
+  openTree($parent.options[i].value);
+};
+
+// Title
 const createTitle = () => {
   const items = [];
   for (const id of ['sort', 'query', 'tag']) {
@@ -73,11 +90,16 @@ const checkTitle = () => {
   isAutoTitle = !$title.value || $title.value === createTitle();
 };
 
+// Load / Save
 const getURLParams = () => {
   const p = new URLSearchParams($bookmarkURL.value.replace(/^place:/, ''));
-  const q = {};
+  const q = { parent: [] };
   for (const [k, v] of p.entries()) {
-    q[k] = v;
+    if (Array.isArray(q[k])) {
+      q[k].push(v);
+    } else {
+      q[k] = v;
+    }
   }
   return q;
 }
@@ -87,8 +109,9 @@ const setBookmarkURL = () => {
   for (const i of document.getElementsByClassName('q')) {
     if (i.value) q[i.id] = i.value;
   }
-  switch (q.parent) {
-    case params.tree[0].id:
+  q.parent = selectedParents;
+  switch (q.parent?.[0]) {
+    case popupArgs.tree[0].id:
       q.type = '1';
       delete q.parent;
       break;
@@ -104,23 +127,31 @@ const setBookmarkURL = () => {
       delete q.type;
       break;
   }
-  const url = 'place:' + (new URLSearchParams(q)).toString();
+  const searchParams = new URLSearchParams();
+  for (const [k, v] of Object.entries(q)) {
+    for (const i of [v].flat()) {
+      searchParams.append(k, i);
+    }
+  }
+  const url = 'place:' + searchParams.toString();
   $bookmarkURL.value = url;
-  return url;
 }
 
 const setFormValues = () => {
   const q = getURLParams();
   for (const [k, v] of Object.entries(q)) {
-    const f = byId(k);
-    if (f) {
-      f.value = v;
-    }
+    (byId(k) ?? {}).value = v;
+  }
+  $parent.selectedIndex = -1;
+  setParentMultiple(1 < q.parent?.length);
+  selectedParents = q.parent;
+  for (const p of q.parent) {
+    (byId(`p-${p}`) ?? {}).selected = true;
   }
   switch (q.type) {
     case '1':
-      if (!q.parent) {
-        $parent.value = params.tree[0].id;
+      if (!q.parent?.[0]) {
+        $parent.value = popupArgs.tree[0].id;
       }
       break;
     case '2':
@@ -130,12 +161,13 @@ const setFormValues = () => {
       $parent.value = 'tags';
       break;
     default:
-      if (!q.parent) {
+      if (!q.parent?.[0]) {
         $parent.value = '';
       }
   }
 }
 
+// Event handler
 let isOnChangeParams = false;
 const onChangeParams = e => {
   if (isOnChangeParams) return;
@@ -155,14 +187,14 @@ const onChangeParams = e => {
 
 const onSubmit = async () => {
   // Do not fix query to save any query.
-  // const url = setBookmarkURL();
+  // setBookmarkURL();
   const bookmark = {
-    parentId: params.bookmark.parentId,
+    parentId: popupArgs.bookmark.parentId,
     title: $title.value,
     url: $bookmarkURL.value,
   };
-  if (params.bookmark?.id) {
-    bookmark.id = params.bookmark.id;
+  if (popupArgs.bookmark?.id) {
+    bookmark.id = popupArgs.bookmark.id;
   }
   await browser.runtime.sendMessage({
     method: 'put',
@@ -171,32 +203,34 @@ const onSubmit = async () => {
   close();
 };
 
-// Event handler
 addEventListener('input', onChangeParams);
 addEventListener('change', onChangeParams);
-$parent.addEventListener('change', openTree);
+$parent.addEventListener('change', onSelectParent);
 $parent.addEventListener('focus', () => $parent.parentNode.scrollTo(0, 0));
 byId('submit').addEventListener('click', onSubmit);
 
 // Main
 const init = async () => {
-  params = await browser.runtime.sendMessage({ method: 'get' });
+  popupArgs = await browser.runtime.sendMessage({ method: 'get' });
 
   // Setup parent tree
-  findParent(params.tree);
   const f = document.createDocumentFragment();
-  addTree(params.tree, f);
+  addTree(popupArgs.tree, f);
   $parent.appendChild(f);
 
   // Setup default parameters
-  $parent.value = params.bookmark.parentId;
-  if (params.bookmark.id) {
-    $title.value = params.bookmark.title;
-    $bookmarkURL.value = params.bookmark.url;
+  if (popupArgs.bookmark.id) {
+    $title.value = popupArgs.bookmark.title;
+    $bookmarkURL.value = popupArgs.bookmark.url;
     setFormValues();
     byId('restartRequired').style.display = 'inline';
   } else {
+    selectedParents = [popupArgs.bookmark.parentId];
+    $parent.value = selectedParents[0];
     setBookmarkURL();
+  }
+  for (const p of selectedParents) {
+    openTree(p).selected = true;
   }
   checkTitle();
 };
